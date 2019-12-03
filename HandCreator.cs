5dc3f95e723f009a2364c122ae927eb2080a02cb
bonachiang <mingtsun17@gmail.com>
2019-12-04 07:06:43 +0800

    add some sort

--- HandCreator.cs
+++ HandCreator.cs
@@ -1,39 +1,48 @@
 class HandCreator
 {
-    Tuple<Hand, Queue<Card>> Arrange(Queue<Card> cards)
+    Tuple<Hand, List<Card>> Arrange(Queue<Card> cards)
     {
-        Tuple<bool, Hand> pairHand = IsStraightFlush
+        Tuple<bool, Hand> pairHand = CheckStraightFlush(cards);
+        if (pairHand.Item1)
+        {
+            Hand hand = new Hand();
+            hand.HandRank = HandRank.StraightFlush;
+            ArrayList<Card> arCards = new ArrayList<Card>();
+            arCards.Add(cards);
+            arCards.Sort();
+            Tuple<Hand, List<Card>> retData = Tuple.Create(hand, );
+        }
     }
 
-    private static Tuple<bool, Hand?> IsStraightFlush(Queue<Card> cards) {
+    private static Tuple<bool, Hand?> CheckStraightFlush(Queue<Card> cards) {
         return true;
     }
 
-    private static Tuple<bool, Hand?> IsStaightFlush(Queue<Card> cards) {
+    private static Tuple<bool, Hand?> CheckStaightFlush(Queue<Card> cards) {
         return true;
     }
 
-    private static Tuple<bool, Hand?> IsFourOfAKind(Queue<Card> cards) {
+    private static Tuple<bool, Hand?> CheckFourOfAKind(Queue<Card> cards) {
         return true;
     }
 
-    private static Tuple<bool, Hand?> IsFullHouse(Queue<Card> cards) {
+    private static Tuple<bool, Hand?> CheckFullHouse(Queue<Card> cards) {
         return true;
     }
 
-    private static Tuple<bool, Hand?> IsFlush(Queue<Card> cards) {
+    private static Tuple<bool, Hand?> CheckFlush(Queue<Card> cards) {
         return true;
     }
 
-    private static Tuple<bool, Hand?> IsThreeOfAKind(Queue<Card> cards) {
+    private static Tuple<bool, Hand?> CheckThreeOfAKind(Queue<Card> cards) {
         return true;
     }
 
-    private static Tuple<bool, Hand?> IsTwoPair(Queue<Card> cards) {
+    private static Tuple<bool, Hand?> CheckTwoPair(Queue<Card> cards) {
         return true;
     }
 
-    private static Tuple<bool, Hand?> IsOnePair(Queue<Card> cards) {
+    private static Tuple<bool, Hand?> CheckOnePair(Queue<Card> cards) {
         return true;
     }
 }
