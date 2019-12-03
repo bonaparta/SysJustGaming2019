f03abc6515935335da3e79eed4de847eac663021
研發部 蔣明村 <bonachiang@inhouse.moneydj.com>
2019-12-03 18:30:31 +0800

    Add Some Methods header only

--- Judge.cs
+++ Judge.cs
@@ -3,4 +3,36 @@ public class Judge
     public string DisplayName { get; set; }
     public Suit Suit { get; set; }
     public int Value { get; set; }
+
+    private bool IsStaightFlush(Player player) {
+        return true;
+    }
+
+    private bool IsStaightFlush(Player player) {
+        return true;
+    }
+
+    private bool IsFourOfAKind(Player player) {
+        return true;
+    }
+
+    private bool IsFullHouse(Player player) {
+        return true;
+    }
+
+    private bool IsFlush(Player player) {
+        return true;
+    }
+
+    private bool IsThreeOfAKind(Player player) {
+        return true;
+    }
+
+    private bool IsTwoPair(Player player) {
+        return true;
+    }
+
+    private bool IsOnePair(Player player) {
+        return true;
+    }
 }
