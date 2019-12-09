259fe15d2cb87568b6a1f1eb78ee4c662a3bbb4e
bonaparta <mingtsun17@gmail.com>
2019-12-09 23:37:13 +0800

    Sim Bugs

--- Program.cs
+++ Program.cs
@@ -8,16 +8,21 @@ namespace SysJustGaming2019
     {
         static void Main(string[] args)
         {
+            PokerGame.Simulator.SimGames.SimTwoCardsInit();
+
             Console.WriteLine("Hello World");
             Deck deck = DeckCreator.CreateCards();
             List<Player> players = new List<Player>();
-            players.Add(new Player("A"));
-            players.Add(new Player("B"));
+            players.Add(new Player("P1"));
+            players.Add(new Player("P2"));
+            players.Add(new Player("P3"));
+            players.Add(new Player("P4"));
             Game game = new Game(deck, players);
 
             Result result = game.Play();
 
-            System.Diagnostics.Debug.WriteLine("Winer: {0} Hand: {1}", result.Winner.Name, result.Winner.Display);
+            //System.Diagnostics.Debug.WriteLine("Winer: {0} Hand: {1}", result.Winner.Name, result.Winner.Display);
+            Console.WriteLine("Winer: {0} Hand: {1}", result.Winner.Name, result.Winner.Display);
         }
     }
 }  // namespace SysJustGaming2019
---
+++ SimGames.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using PokerGame;
+
+namespace PokerGame.Simulator
+{
+public class SimGames
+{
+    private static int s_nTimesPerSim = 1000000;
+
+    public static void SimTwoCardsInit()
+    {
+        Deck deck = DeckCreator.CreateCards();
+        List<Player> players = new List<Player>();
+        for (int i = 0; i < 4; ++i)
+        {
+            players.Add(new Player("P" + i.ToString()));
+            players[i].Cards = new List<Card>();
+        }
+
+        Card card = deck.HaveACardUpMySleeve(14, Suit.Spades);
+        players[0].AddCard(card);
+        card = deck.HaveACardUpMySleeve(14, Suit.Hearts);
+        players[0].AddCard(card);
+        
+        deck.Save();
+        for (int i = 0; i < players.Count; ++i)
+            players[i].Save();
+        
+        int nWin = 0;
+        for (int i = 0; i < s_nTimesPerSim; ++i)
+        {
+            deck.Load();
+            for (int j = 0; j < players.Count; ++j)
+                players[i].Load();
+            
+            Game game = new Game(deck, players);
+            Result result = game.Play();
+            
+            if (result.Winner == players[0])
+                ++nWin;
+        }
+
+        players[0].Load();
+        System.Diagnostics.Debug.WriteLine("Player: {0} Win: {1}", players[0].Display, (double)nWin / s_nTimesPerSim);
+    }
+}
+}
